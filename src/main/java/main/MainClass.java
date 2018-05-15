456035abef76658297d9760a6ae58f2620d26329
pnguyen9 <nguyen.pascal@outlook.fr>
2018-05-15 16:20:53 +0200

    Fixing merge conflict in Main

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD:src/main/java/MainClass.java
 
-
-public class MainClass {
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-}
-=======
 package main;
 
 public class MainClass {
@@ -15,4 +7,3 @@ public class MainClass {
 		System.out.println("Test");
 	}
 }
->>>>>>> 256071fab49e64c2a263f1ac826c77624e02f6fb:src/main/java/main/MainClass.java
